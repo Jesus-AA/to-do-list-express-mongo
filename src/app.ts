9d9a8b3ce5683d30763ce6ae7da19b398b43916c
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-10-11 16:56:00 +0200

    Add app and dbconnect files

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+
+export const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req: Request, res: Response) => {
+  res.write('<h1>To-Do-List-Server</h1>');
+  res.end();
+});
+
+app.use('/todolist');
