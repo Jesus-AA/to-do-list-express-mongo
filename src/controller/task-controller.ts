8f9a418e0fd277c42f44a154a9391306cfa2eee1
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-10-11 22:01:49 +0200

    Add task-controller file

---
+++ src/controller/task-controller.ts
@@ -0,0 +1,65 @@
+import createDebug from 'debug';
+import { NextFunction, Request, Response } from 'express';
+import { Task } from '../entities/task';
+import { Repository } from '../repository/repository';
+import { UserMongoRepository } from '../repository/user-mongo-repository';
+import { Controller } from './controller';
+
+const debug = createDebug('TDL10:TaskController');
+
+export class TaskController extends Controller<Task> {
+  constructor(protected repo: Repository<Task>) {
+    super(repo);
+    debug('Instantiated');
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userRepo = new UserMongoRepository();
+
+      const user = await userRepo.getById(req.body.author);
+
+      const finalTask = await this.repo.create(req.body);
+
+      user.tasks.push(finalTask);
+
+      userRepo.update(user.id, user);
+
+      const finalTask2 = await this.repo.getById(finalTask.id);
+
+      res.status(201);
+      res.json(finalTask2);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const task = await this.repo.getById(req.params.id);
+      const userId = task.author.id;
+      const userRepo = new UserMongoRepository();
+      const user = await userRepo.getById(userId);
+      const newTaskArray = user.tasks.filter(
+        (task) => String(task) !== req.params.id
+      );
+      user.tasks = newTaskArray;
+      await this.repo.delete(req.params.id);
+      userRepo.update(user.id, user);
+      res.status(204);
+      res.json({});
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const updatedTask = await this.repo.update(req.params.id, req.body);
+      res.status(200);
+      res.json(updatedTask);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
